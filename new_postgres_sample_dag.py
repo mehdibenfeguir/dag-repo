7d508c12e1b59075910dfac0393e718daf06d658
mehdibenfeguir <mehdi.benfeguir@gmail.com>
2022-02-10 15:30:59 +0300

    Update new_postgres_sample_dag.py

--- new_postgres_sample_dag.py
+++ new_postgres_sample_dag.py
@@ -159,7 +159,7 @@ def create_es_publisher_sample_job():
     job.launch()
 
 
-with DAG('amundsen_databuilder', default_args=default_args, **dag_args) as dag:
+with DAG('amundsen_databuilder_gcp_neo4j', default_args=default_args, **dag_args) as dag:
     postgres_table_extract_job = PythonOperator(
         task_id='postgres_table_extract_job',
         python_callable=create_table_extract_job
