64900fc9ec4bb8eb63d2ed636ed0d567b599fc20
mehdibenfeguir <mehdi.benfeguir@gmail.com>
2022-02-13 11:44:22 +0300

    Update new_postgres_sample_dag.py

--- new_postgres_sample_dag.py
+++ new_postgres_sample_dag.py
@@ -65,15 +65,12 @@ es = Elasticsearch([
 ])
 
 # TODO: user provides a list of schema for indexing
- SUPPORTED_SCHEMAS = ['public']
+# SUPPORTED_SCHEMAS = ['public']
 # # String format - ('schema1', schema2', .... 'schemaN')
- SUPPORTED_SCHEMA_SQL_IN_CLAUSE = "('{schemas}')".format(schemas="', '".join(SUPPORTED_SCHEMAS))
+# SUPPORTED_SCHEMA_SQL_IN_CLAUSE = "('{schemas}')".format(schemas="', '".join(SUPPORTED_SCHEMAS))
+# OPTIONAL_TABLE_NAMES = 'applications'
 
- OPTIONAL_TABLE_NAMES = 'applications'
-
-connection = BaseHook.get_connection('bigquery_connection')
-
-        
+connection = BaseHook.get_connection('bigquery_connection')  
 def connection_string():
     user = connection.login
     password = connection.password
